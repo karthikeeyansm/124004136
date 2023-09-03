1d95f2ca3f334751eccb7810ed438c6e6dc0191f
karthikeeyansm <karthi2002smk@gmail.com>
2023-09-03 11:58:40 +0530

    output uploaded

--- train/api/views.py
+++ train/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 import requests
 
+import datetime 
+
 # Create your views here.
 
 token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTM3MTkyOTksImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiMzQwMWVjZDktZTBmMC00NTRkLTlhMGMtNDUzOGUwYzVhYWU0Iiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjEyNDAwNDEzNiJ9.fElu81AhZq9LHK8YrPCQ5u_V5-2ZTLiDNSW41Za5ZaM"
@@ -30,7 +32,24 @@ def home(request):
             print(token)
     except:
         pass
+
+
     response=requests.get(url,headers=headers)
     response_data=response.json()
     print(response_data)
-    return JsonResponse(response.json(),safe=False)
+
+
+    filtered_data=[]
+
+    for tr in response_data:
+        # print(tr)
+        time=tr['departureTime']
+        now=datetime.datetime.now()
+        train_departTime=datetime.datetime(hour=time['Hours'],minute=time['Minutes'],second=time['Seconds'],year=now.year,day=now.day,month=now.month)
+
+        time_difference=train_departTime-now
+        if(time_difference.total_seconds()<12*3600):
+            filtered_data.append(tr)
+
+    print(filtered_data)
+    return JsonResponse(filtered_data,safe=False)
